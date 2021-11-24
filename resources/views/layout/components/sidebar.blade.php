50908e88ffe7c072ee8e3e16e2da748fbf9812bb
Gilbert de Oliveira Santos <gilberticm8@gmail.com>
2021-11-24 00:47:52 -0300

    Ajustes na sidebar
    Alterações nos links de navegação do sidebar.

--- resources/views/layout/components/sidebar.blade.php
+++ resources/views/layout/components/sidebar.blade.php
@@ -16,7 +16,8 @@
                    with font-awesome or any other icon font library -->
 
                 <li class="nav-item">
-                    <a href="{{route('dashboard.home')}}" class="nav-link @if(Route::current()->getName() == 'dashboard.home')) active @endif">
+                    <a href="{{route('dashboard.home')}}"
+                       class="nav-link @if(Route::current()->getName() == 'dashboard.home')) active @endif">
                         <i class="nav-icon fas fa-chart-pie"></i>
                         <p>
                             Dashboard
@@ -25,7 +26,8 @@
                 </li>
 
                 <li class="nav-item">
-                    <a href="{{route('dashboard.users')}}" class="nav-link @if(Route::current()->getName() == 'dashboard.users')) active @endif">
+                    <a href="{{route('dashboard.users')}}"
+                       class="nav-link @if(Route::current()->getName() == 'dashboard.users')) active @endif">
                         <i class="nav-icon fas fa-users-cog"></i>
                         <p>
                             Usuários
@@ -34,7 +36,8 @@
                 </li>
 
                 <li class="nav-item">
-                    <a href="{{route('dashboard.customers')}}" class="nav-link @if(Route::current()->getName() == 'dashboard.customers') active @endif">
+                    <a href="{{route('dashboard.customers')}}"
+                       class="nav-link @if(Route::current()->getName() == 'dashboard.customers') active @endif">
                         <i class="nav-icon fas fa-users"></i>
                         <p>
                             Clientes
@@ -43,7 +46,8 @@
                 </li>
 
                 <li class="nav-item @if(Route::current()->getPrefix() == 'dashboard/tickets') menu-open @endif">
-                    <a href="#" class="nav-link @if(Route::current()->getPrefix() == 'dashboard/tickets') active @endif">
+                    <a href="#"
+                       class="nav-link @if(Route::current()->getPrefix() == 'dashboard/tickets') active @endif">
                         <i class="nav-icon fas fa-ticket-alt"></i>
                         <p>
                             Tickets
@@ -52,26 +56,31 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="{{route('dashboard.tickets.new')}}" class="nav-link @if(Route::current()->getName() == 'dashboard.tickets.new')) active @endif">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Novo Ticket</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{route('dashboard.tickets.open')}}" class="nav-link @if(Route::current()->getName() == 'dashboard.tickets.open')) active @endif">
+                            <a href="{{route('dashboard.tickets.open')}}"
+                               class="nav-link @if(Route::current()->getName() == 'dashboard.tickets.open')) active @endif">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Tickets Abertos</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{route('dashboard.tickets.finished')}}" class="nav-link @if(Route::current()->getName() == 'dashboard.tickets.finished')) active @endif">
+                            <a href="{{route('dashboard.tickets.finished')}}"
+                               class="nav-link @if(Route::current()->getName() == 'dashboard.tickets.finished')) active @endif">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Tickets Finalizados</p>
                             </a>
                         </li>
 
                         <li class="nav-item">
-                            <a href="{{route('dashboard.tickets.categories')}}" class="nav-link @if(Route::current()->getName() == 'dashboard.tickets.categories')) active @endif">
+                            <a href="{{route('dashboard.tickets.new')}}"
+                               class="nav-link @if(Route::current()->getName() == 'dashboard.tickets.new')) active @endif">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Novo Ticket</p>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="{{route('dashboard.tickets.categories')}}"
+                               class="nav-link @if(Route::current()->getName() == 'dashboard.tickets.categories')) active @endif">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Categorias</p>
                             </a>
