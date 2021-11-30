3d236c1f0e8e19214cee7140bbd68b55380d4b7c
Gilbert de Oliveira Santos <gilberticm8@gmail.com>
2021-11-30 02:55:59 -0300

    Ajuste na mascara de listagem de clientes.

--- resources/views/dashboard/customers.blade.php
+++ resources/views/dashboard/customers.blade.php
@@ -217,7 +217,7 @@
         var masks = ['000.000.000-000', '00.000.000/0000-00'];
 
         let tableDocument = $('.table-cpf-cnpj')
-        tableDocument.mask((document.length = 14) ? masks[1] : masks[0]);
+        tableDocument.mask((document.length = 11) ? masks[1] : masks[0]);
 
         $("input[id*='document']").inputmask({
             mask: ['999.999.999-99', '99.999.999/9999-99'],
