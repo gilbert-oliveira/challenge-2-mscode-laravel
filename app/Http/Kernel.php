daafbed660a10b6cf47dd87c856b36b0adb5cfc6
Gilbert de Oliveira Santos <gilberticm8@gmail.com>
2021-11-30 00:13:48 -0300

    Adicionada as middlewares no kernel da aplicação.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'check.active' => \App\Http\Middleware\CheckUserActive::class,
+        'check.admin' => \App\Http\Middleware\CheckAdminUser::class,
     ];
 }
