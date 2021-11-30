c0ac049c39ce40b076ef670c28192db4781dbce0
Gilbert de Oliveira Santos <gilberticm8@gmail.com>
2021-11-30 00:28:16 -0300

    Ajuste na fábrica de Usuários.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,22 +2,33 @@
 
 namespace Database\Factories;
 
+use App\Models\Team;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Laravel\Jetstream\Features;
 
 class UserFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
     /**
      * Define the model's default state.
      *
      * @return array
      */
-    public function definition(): array
+    public function definition()
     {
         return [
             'name' => $this->faker->unique()->name(),
             'cpf' => rand(10000000000, 99999999999),
             'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
             'master' => false,
             'active' => $this->faker->boolean(),
             'password' => password_hash('123456', PASSWORD_DEFAULT), // password
@@ -28,14 +39,34 @@ class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      *
-     * @return Factory
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
      */
-    public function unverified(): Factory
+    public function unverified()
     {
         return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
             ];
         });
+    }
+
+    /**
+     * Indicate that the user should have a personal team.
+     *
+     * @return $this
+     */
+    public function withPersonalTeam()
+    {
+        if (! Features::hasTeamFeatures()) {
+            return $this->state([]);
+        }
+
+        return $this->has(
+            Team::factory()
+                ->state(function (array $attributes, User $user) {
+                    return ['name' => $user->name.'\'s Team', 'user_id' => $user->id, 'personal_team' => true];
+                }),
+            'ownedTeams'
+        );
     }
 }
