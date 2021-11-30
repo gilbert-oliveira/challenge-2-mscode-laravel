78ad9079d491afd73ed656c19105c8b623b64c67
Gilbert de Oliveira Santos <gilberticm8@gmail.com>
2021-11-30 00:03:08 -0300

    View para resposta de erros da aplicação.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{config('app.locale')}}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>404</title>
+
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Passion+One" rel="stylesheet">
+
+    <!-- Font Awesome Icon -->
+    <link type="text/css" rel="stylesheet" href="{{asset('errors/css/font-awesome.min.css')}}"/>
+
+    <!-- Custom stlylesheet -->
+    <link type="text/css" rel="stylesheet" href="{{asset('errors/css/style.css')}}"/>
+
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+</head>
+
+<body>
+
+<div id="notfound">
+    <div class="notfound-bg"></div>
+    <div class="notfound">
+        <div class="notfound-404">
+            <h1>404</h1>
+        </div>
+        <h2>Opa! Página Não Encontrada</h2>
+    </div>
+</div>
+
+</body>
+
+</html>
