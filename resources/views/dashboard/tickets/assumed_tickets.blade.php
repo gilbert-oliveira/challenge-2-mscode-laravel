0ab4555f4afea23f9a196721af909a030b8f6426
Gilbert de Oliveira Santos <gilberticm8@gmail.com>
2021-11-30 00:02:10 -0300

    View para listagem de tickets assumidos.

---
+++ resources/views/dashboard/tickets/assumed_tickets.blade.php
@@ -0,0 +1,179 @@
+@extends('layout.template')
+
+@section('title', 'Tickets Assumidos')
+
+@section('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.bootstrap4.min.css">
+@stop
+
+@section('breadcrumb')
+    <li class="breadcrumb-item"><a href="#">Tickets</a></li>
+    <li class="breadcrumb-item active">Assumidos</li>
+@stop
+
+@section('content')
+
+    <div class="card">
+        <!-- /.card-header -->
+        <div class="card-body">
+            <table id="usuarios" class="table table-sm table-bordered table-striped ">
+                <thead>
+                <tr>
+                    <th>Nº</th>
+                    <th>Titúlo</th>
+                    <th>Categoria</th>
+                    <th>Criado em</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+
+                @foreach($assumedTickets as $ticket)
+
+                    <tr>
+                        <td>{{$ticket->id}}</td>
+                        <td>{{$ticket->title}}</td>
+                        <td>{{$ticket->category()->name}}</td>
+                        <td>{{$ticket->created_at}}</td>
+                        <td class="text-center">
+                            <div class="row">
+                                <div class="col">
+                                    <a class="btn btn-info btn-sm w-100 assumed-ticket"
+                                       href="{{route('dashboard.tickets.details', $ticket->id)}}">
+                                        <i class="fas fa-info-circle"></i> Detalhes
+                                    </a>
+                                </div>
+                                <div class="col">
+                                    <form method="POST" id="finish-ticket"
+                                          action="{{route('dashboard.tickets.finish')}}">
+
+                                        @csrf
+
+                                        <input type="hidden" id="input-finish-ticket" name="id"
+                                               value="">
+
+                                        <a class="btn btn-secondary btn-sm w-100 finish-ticket"
+                                           data-id="{{$ticket->id}}">
+                                            <i class="fas fa-user-tag"></i> Finalizar
+                                        </a>
+                                    </form>
+                                </div>
+                                <div class="col">
+                                    <button type="button" data-toggle="modal" data-id="{{$ticket->id}}"
+                                            data-target="#transfer-ticket"
+                                            class="btn btn-success transfer-ticket">
+                                        Transferir Ticket
+                                    </button>
+                                </div>
+                            </div>
+
+                        </td>
+                    </tr>
+                @endforeach()
+                </tbody>
+            </table>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+@stop
+
+@section('modals')
+    <!-- Modal transferir ticket -->
+    <div class="modal fade" id="transfer-ticket" tabindex="-1" aria-labelledby="tranfer-ticket"
+         aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form method="POST" action="{{ route('dashboard.tickets.transfer') }}">
+                    @csrf
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="transfer-ticket">Transferir Ticket</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+
+                        <div class="form-group">
+                            <label for="public">Usuário</label>
+                            <select class="custom-select" id="users_id" name="users_id">
+                                @foreach(\App\Models\User::all() as $user)
+                                    @if($user->id != auth()->user()->id)
+                                        <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                    @endif
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <input type="hidden" id="ticket_id" name="ticket_id" value="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar
+                        </button>
+                        <button type="submit" class="btn btn-primary">Transferir</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('scripts')
+    {{-- jquery DataTables --}}
+    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+
+    {{-- DataTables --}}
+    <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.bootstrap4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.7.1/jszip.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/pdfmake.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.colVis.min.js"></script>
+
+    {{-- Locais --}}
+    <script src="{{asset('js/dashboard/usuarios/datatable.js')}}"></script>
+
+    <script>
+        $('.finish-ticket').on('click', function () {
+            // recupera o data-id
+            let id = $(this).data('id');
+
+            // insere Id(id);
+            $('input[id=input-finish-ticket]').val(id);
+
+            // Mensagem de confirmação
+            Swal.fire({
+                title: 'Deseja finalizar o ticket?',
+                text: "Após finalizar o ticket o cliente será notificado e ficará sobre sua responsabilidade!",
+                icon: 'info',
+                showCancelButton: true,
+                confirmButtonColor: '#4CA847',
+                cancelButtonColor: '#424242',
+                cancelButtonText: 'Cancelar',
+                confirmButtonText: 'Sim, Finalizar!'
+            }).then((result) => {
+                // Confirma a exclusão
+                if (result.isConfirmed) {
+                    //Recupera o formulário
+                    let form = $('#finish-ticket');
+                    //Envia o formulário
+                    form.submit();
+                }
+            })
+        });
+    </script>
+
+    <script>
+        $('.transfer-ticket').on('click', function () {
+            let id = $(this).data('id');
+            $('#ticket_id').val(id)
+            console.log($('#ticket_id').val());
+        })
+    </script>
+@stop
