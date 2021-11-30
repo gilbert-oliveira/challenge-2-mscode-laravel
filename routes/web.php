12648a38d6f7689b775ccb34e9d13952ad88d65f
Gilbert de Oliveira Santos <gilberticm8@gmail.com>
2021-11-29 21:58:36 -0300

    Adicionada novas rotas da aplicação.

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\{
     UsersController,
 };
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,12 +25,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 //Rota para a página inicial do dashboard
-Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
+Route::middleware(['auth:sanctum', 'check.active'])->get('/', function () {
     return redirect(route('dashboard.home'));
 });
 
+// Rota publica para visualizar um ticket
+Route::get('/ticket/{token}', [TicketController::class, 'getDetailsPublic'])->name('public.ticket.details');
+
+//Rota publica para cadastro de uma observação
+Route::post('/ticket/{token}/observação/cadastrar', [ObservationController::class, 'postPublicObservation'])->name('public.observation.new');
+
 // Grupo de rotas para o Dashboard
-Route::prefix('dashboard')->middleware(['auth:sanctum', 'verified'])->group(function () {
+Route::prefix('dashboard')->middleware(['auth:sanctum', 'check.active'])->group(function () {
 
     //Rota para a página inicial do Dashboard
     Route::get('/', function () {
@@ -39,8 +46,14 @@ Route::prefix('dashboard')->middleware(['auth:sanctum', 'verified'])->group(func
     //Rota para a página inicial do Dashboard
     Route::get('home', [HomeController::class, 'getHome'])->name('dashboard.home');
 
+    //Rota para tela de alterar a senha
+    Route::get('alterar-senha', [UsersController::class, 'getChangePassword'])->name('dashboard.change-password');
+    //Rota para alterar a senha
+    Route::post('alterar-senha', [UsersController::class, 'postChangePassword'])->name('dashboard.change-password');
+
+
     // Grupo de rotas para usuários
-    Route::prefix('usuarios')->group(function () {
+    Route::prefix('usuarios')->middleware(['check.admin'])->group(function () {
 
         // Rota para listagem de usuários
         Route::get('/', [UsersController::class, 'getUsers'])->name('dashboard.users');
@@ -59,6 +72,8 @@ Route::prefix('dashboard')->middleware(['auth:sanctum', 'verified'])->group(func
     Route::get('clientes', [CustomerController::class, 'getCustomers'])->name('dashboard.customers');
     // Rota para cadastro de clientes
     Route::post('clientes', [CustomerController::class, 'postCustomers'])->name('dashboard.customers');
+    // Rota para alteração de clientes
+    Route::put('clientes', [CustomerController::class, 'putCustomers'])->name('dashboard.customers');
 
     // Rota para deletar o attachment
     Route::delete('attachment/deletar/{id}', [AttachmentController::class, 'deleteAttachment'])->name('dashboard.attachment.delete');
@@ -67,23 +82,41 @@ Route::prefix('dashboard')->middleware(['auth:sanctum', 'verified'])->group(func
     Route::prefix('tickets')->group(function () {
 
         // Rota para detalhes de um ticket
-        Route::get('detalhes/{id}', [HomeController::class, 'detailTicket'])->name('dashboard.tickets.details');
+        Route::get('detalhes/{id}', [TicketController::class, 'detailTicket'])->name('dashboard.tickets.details');
 
         // Rota para listagem de tickets abertos
         Route::get('abertos', [TicketController::class, 'getOpenTickets'])->name('dashboard.tickets.open');
 
+        //Rota para Tickets assumidos
+        Route::get('assumidos', [TicketController::class, 'getAssumedTickets'])->name('dashboard.tickets.assumed');
+
         // Rota para listagem de tickets finalizados
-        Route::get('finalizados', [TicketController::class, 'getOpenTickets'])->name('dashboard.tickets.finished');
+        Route::get('finalizados', [TicketController::class, 'getFinishedTickets'])->name('dashboard.tickets.finished');
 
         // Rota para tela de cadastro de tickets
         Route::get('criar', [TicketController::class, 'newTickets'])->name('dashboard.tickets.new');
         // Rota para cadastro de tickets
         Route::post('criar', [TicketController::class, 'postTickets'])->name('dashboard.tickets.new');
 
+        // Rota para assumir tickets
+        Route::post('assumir', [TicketController::class, 'assumeTicket'])->name('dashboard.tickets.assume');
+        // Rota para finalizar tickets
+        Route::post('finalizar', [TicketController::class, 'finishTicket'])->name('dashboard.tickets.finish');
+
+        // Rota para reabir tickets
+        Route::post('reabrir', [TicketController::class, 'reopenTicket'])->name('dashboard.tickets.reopen');
+
+        // Rota para transferir tickets
+        Route::post('transferir', [TicketController::class, 'transferTicket'])->name('dashboard.tickets.transfer');
+
         // Rota para tela de cadastro de categorias de tickets
         Route::get('categorias', [CategoryController::class, 'getCategories'])->name('dashboard.tickets.categories');
         // Rota para cadastro de categorias de tickets
         Route::post('categorias', [CategoryController::class, 'postCategories'])->name('dashboard.tickets.categories');
+        // Rota para deletar uma categoria de tickets
+        Route::delete('categorias/deletar/{id}', [CategoryController::class, 'deleteCategory'])->name('dashboard.tickets.categories.delete');
+        // Rota para editar uma categoria de tickets
+        Route::put('categorias/editar/{id}', [CategoryController::class, 'editCategory'])->name('dashboard.tickets.categories.edit');
 
         // Rota para cadastro de observações de um ticket
         Route::post('observacao/cadastrar', [ObservationController::class, 'postObservation'])->name('dashboard.tickets.observation');
